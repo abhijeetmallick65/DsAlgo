e437ac38f12a4b6d40c9df85123f660f087e7791
abhijeetmallick65 <abhijeetmallick29@gmail.com>
2021-06-01 13:02:15 +0530

    Linked list operations

--- LinkedList/main.cpp
+++ LinkedList/main.cpp
@@ -27,12 +27,15 @@ void create(int A[],int n){
     {
         /* code */
         // temp = new Node;
-
-
+        // new node
         temp = (struct Node *)malloc(sizeof(struct Node));
+        // add data
         temp->data = A[i];
+        // next is null
         temp->next = NULL;
+        // connect to previous
         last->next = temp;
+        // move to latest *last*
         last = temp;
         i++;
     }
@@ -49,7 +52,13 @@ void Display(struct Node *p){
     }
     // cout << head->data ;
 }
-
+void Rdisplay(struct Node *p){
+    if(p!=NULL){
+        // Rdisplay(p->next);
+        cout << p->data << endl;
+        Rdisplay(p->next);
+    }
+}
 // Linked list operations
 int count(){
     struct Node *p = head;
@@ -103,13 +112,105 @@ int Rmax(struct Node *p){
         return p->data;
     }
 }
-int main(){
-    int A[] ={3,4,5,6,7};
-    create(A,5);
-    Display(head);
-    cout << max(head) << endl ;
-    cout << Rmax(head) ;
+// 1/6/21
+struct Node * Search(struct Node *p,int key){
+    while(p != NULL){
+        if(p->data == key)return p;
+        p = p->next;
+    }
+    return NULL;
+}
+struct Node * Rsearch(struct Node *p,int key){
+    if(p == NULL)return NULL;
+    if(p->data == key)return p;
+    return Rsearch(p->next,key);
+}
+struct Node * moveToHead(struct Node *p,int key){
+    struct Node *q = NULL;
+    while(p){
+        if(p->data == key){
+            q->next = p->next;
+            p->next = head;
+            head = p;
+            return p;
+        }
+        q = p;
+        p= p->next;
+    }
+    return NULL;
+}
+// insert 
+void Insert(struct Node *p,int index,int val){
+    struct Node *t = NULL;
+    // insert at start
+    if(index == 0){
+        t = new Node;
+        t->data = val;
+        t->next = head;
+        head = t;
+    }else if(index>0){
+        // any other index
+        for(int i = 0;i< index-1 && p;i++){
+            p = p->next;
+        }
+        if(p){
+            t= new Node;
+            t->data = val;
+            t->next = p->next;
+            p->next = t;
+        }
+    }
+}
+void insertLast(int value){
+    struct Node *t = (struct Node *)malloc(sizeof(struct Node));
+    struct Node *last;
+    t->data = value;
+    t->next = NULL;
+    if(head == NULL){
+        head = last  = t;
+    }else{
+        last = head;
+        while(last->next){
+            last = last->next;
+        }
+        last->next = t;
+        last = t;
+    }
+}
+void sortedInsert(struct Node *p,int val){
+    // creating node to be inserted
+    struct Node *t = new Node;
+    struct Node *q = NULL;
     
+    t->data = val;
+    t->next = NULL;
+
+    // if(head == null) / t is the first node
+    if(head == NULL){
+        head = t;
+    }else{
+        while(p && p->data < val){
+            q = p;
+            p = p->next;
+        }
+        // if val < head->data
+        if(p == head){
+            t->next = head;
+            head = t;
+        }else{
+            // val > head->data
+            t->next = q->next;
+            q->next = t;
+        }
+    }
+}
+int main(){
+    int A[] ={3,4,5,7,8};
+    // create(A,5);
+    sortedInsert(head,6);
+    sortedInsert(head,2);
+    sortedInsert(head,12);
+    Rdisplay(head);
     return 0;
 }
 /*
