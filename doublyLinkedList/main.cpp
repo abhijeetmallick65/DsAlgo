93e1e195f0b0ba705c6958b40acfe39ab9bf8cd9
abhijeetmallick65 <abhijeetmallick29@gmail.com>
2021-06-04 13:34:26 +0530

    Doubly Linked List

--- doublyLinkedList/main.cpp
+++ doublyLinkedList/main.cpp
@@ -34,9 +34,9 @@ void create(int A[],int n){
         last = temp;
     }
 }
-
 // Length
 int Length(struct Node *p){
+    if(!head)return 0;
     int count = 0;
     while(p!=NULL){
         count++;
@@ -44,7 +44,51 @@ int Length(struct Node *p){
     }
     return count;
 }
+// insert
+void Insert(struct Node *p,int pos,int val){
+    if(pos<0 || pos> Length(head))return;
 
+    struct Node *temp;
+    temp = new Node;
+    temp->data = val;
+    if(pos == 0){
+        temp->prev = NULL;
+        temp->next = head;
+        head->prev = temp;
+        head = temp;
+    }else{
+        for(int i = 0;i<pos-1;i++){
+            p = p->next;
+        }
+        temp->next = p->next;
+        temp->prev = p;
+        if(p->next){
+            p->next->prev = temp;
+        }
+        p->next = temp;
+    }
+}
+// Delete
+int Delete(struct Node *p,int index){
+    if(index <= 0 || index > Length(head))return -1;
+    int x = 0;
+    if(index == 1){
+        p = head;
+        head = head->next;
+        if(head != NULL)head->prev = NULL;
+        x = p->data;
+        delete p;
+    }else{
+        for(int i = 0;i<index-1;i++){
+            p = p->next;
+        }
+        p->prev->next = p->next;
+        if(p->next)p->next->prev = p->prev;
+        x = p->data;
+        delete p;
+    }
+    return x;
+}
 // Display
 void Display(struct Node *p){
     while(p!=NULL){
@@ -53,11 +97,10 @@ void Display(struct Node *p){
     }
 }
 
-
 int main(){
     int A[] = {1,2,3,4,5};
     create(A,5);
+    Delete(head,5);
     Display(head);
-    cout << Length(head) << endl;
     return 0;
 }
