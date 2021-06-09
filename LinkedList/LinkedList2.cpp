20c3845d4b35a237b34fd3caea2fa59606136ded
abhijeetmallick65 <abhijeetmallick29@gmail.com>
2021-06-09 16:54:39 +0530

    LL class

--- LinkedList/LinkedList2.cpp
+++ LinkedList/LinkedList2.cpp
@@ -3,27 +3,63 @@
 using namespace std;
 
 class Node{
-    private:
+    public:
     int data;
     Node *next;
+    Node(int data){
+        this->data = data;
+        this->next = NULL; 
+    }
 };
 
 class LinkedList{
     private:
     Node *head;
-
     public:
     LinkedList(){
         head = NULL;
     }
-    LinkedList(int a[],int n);
-    ~LinkedList();
-
-    void Display();
-    void Insert(int data,int x);
-    int Delete(int index);
-    int Length();
+    LinkedList(int a[],int n){
+        head = new Node(a[0]);
+        Node *last = head;
+        for(int i = 1;i<n;i++){
+            Node *temp = new Node(a[i]);
+            last->next = temp;
+            last = temp;
+        }
+    }
+    int remove(int x){
+        if(!this->head)return -1;
+        int data;
+        if(x == 1){
+            Node *temp = head;
+            head = head->next;
+            data = temp->data;
+            delete temp;
+        }else{
+            Node *temp = head;
+            for(int i = 0;i<x-2;i++)
+                temp=temp->next;
+            Node *req = temp->next;
+            temp->next = req->next ? req->next : NULL;
+            data = req->data;
+            delete req;
+        }
+        return x;
+    }
+    void Display(){
+        Node *p = head;
+        while(p!=NULL){
+            cout << p->data << " ";
+            p=p->next;
+        }
+    }
 };
+
 int main(){
+    int a[] = {1,2,3,4,5};
+    LinkedList ll(a,5);
+    ll.remove(8);
+    ll.Display();
     return 0;
 }
